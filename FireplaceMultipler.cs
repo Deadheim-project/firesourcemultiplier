e7cf6c96eed0ea8165514146cc882d8b27ae7e5e
werner <werneralexandre@outlook.com>
2021-10-18 09:48:43 -0300

    fireplacemultiplier

---
+++ FireplaceMultipler.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace FireplaceMultipler
+{
+    [BepInPlugin("Detalhes.FireplaceMultipler", "FireplaceMultipler", "1.0.2")]
+    [BepInProcess("valheim.exe")]
+    public class FireplaceMultipler : BaseUnityPlugin
+    {
+        public const string PluginGUID = "Detalhes.FireplaceMultipler";
+        Harmony harmony = new Harmony(PluginGUID);
+
+        public static ConfigEntry<int> multiplier;
+
+        private void Awake()
+        {
+            multiplier = Config.Bind<int>("General", "Multiplier", 5, "Firesource multiplier");
+
+            harmony.PatchAll();
+        }      
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace FireplaceMultipler
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(Fireplace), "Awake")]
+        public static class ConsumePatch
+        {
+            private static void Prefix(Fireplace __instance)
+            {
+                __instance.m_maxFuel *= FireplaceMultipler.multiplier.Value;
+            }      
+        }
+    }
+}
